f155d65b4a70f8162a8466634dc992b7f64149b1
Marek Gogol <info@marekgogol.sk>
2025-03-05 13:35:39 +0100

    refactor

--- src/Auth/auth.php
+++ src/Auth/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use AdminHelpers\Auth\Models\Otp\WhitelistedToken;
+
 function hasOtpEnabled()
 {
     return config('admin_helpers.auth.otp.enabled', false) === true;
@@ -8,4 +10,38 @@ function hasOtpEnabled()
 function otpModel()
 {
     return Admin::getModelByTable('otp_tokens');
+}
+
+if ( !function_exists('isTokenDebug') ) {
+    /**
+     * Check if the token's are in development mode
+     *
+     * @return bool
+     */
+    function isTokenDebug()
+    {
+        return app()->hasDebugModeEnabled() && isTestEnvironment() && env('TOKEN_DEBUG', false) === true;
+    }
+}
+
+if ( !function_exists('isTestIdentifier') ) {
+    function isTestIdentifier($identifier, $token = null)
+    {
+        // Enable log in only with whitelisted tokens
+        if ( $token && in_array($token, config('admin_helpers.auth.otp.whitelisted_tokens', [])) === false ) {
+            return false;
+        }
+
+        // All tokens are whitelisted
+        if ( isTokenDebug() === true ){
+            return true;
+        }
+
+        // Check if some token is whitelisted
+        if ( in_array($identifier, WhitelistedToken::getParsedIdentifiers()) ){
+            return true;
+        }
+
+        return in_array($identifier, config('admin_helpers.auth.otp.test_identifiers', []));
+    }
 }
--- src/Utilities/helpers.php
+++ src/Utilities/helpers.php
@@ -1,7 +1,5 @@
 <?php
 
-use AdminHelpers\Auth\Models\Otp\WhitelistedToken;
-
 if ( !function_exists('isTestEnvironment') ) {
     /**
      * Check if the environment is test
@@ -11,40 +9,5 @@ if ( !function_exists('isTestEnvironment') ) {
     function isTestEnvironment()
     {
         return app()->environment(['local', 'stagging']);
-    }
-}
-
-
-if ( !function_exists('isTokenDebug') ) {
-    /**
-     * Check if the token's are in development mode
-     *
-     * @return bool
-     */
-    function isTokenDebug()
-    {
-        return app()->hasDebugModeEnabled() && isTestEnvironment() && env('TOKEN_DEBUG', false) === true;
-    }
-}
-
-if ( !function_exists('isTestIdentifier') ) {
-    function isTestIdentifier($identifier, $token = null)
-    {
-        // Enable log in only with whitelisted tokens
-        if ( $token && in_array($token, config('admin_helpers.auth.otp.whitelisted_tokens', [])) === false ) {
-            return false;
-        }
-
-        // All tokens are whitelisted
-        if ( isTokenDebug() === true ){
-            return true;
-        }
-
-        // Check if some token is whitelisted
-        if ( in_array($identifier, WhitelistedToken::getParsedIdentifiers()) ){
-            return true;
-        }
-
-        return in_array($identifier, config('admin_helpers.auth.otp.test_identifiers', []));
     }
 }
