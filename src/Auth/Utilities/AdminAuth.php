0b8535ca99a7c17c55aeff33965d1a79c196a23b
Marek Gogol <info@marekgogol.sk>
2025-03-05 09:46:42 +0100

    comments added

--- src/Auth/Utilities/AdminAuth.php
+++ src/Auth/Utilities/AdminAuth.php
@@ -9,46 +9,101 @@ use AdminHelpers\Auth\Controllers\RegisterController;
 
 class AdminAuth
 {
+    /**
+     * Login routes with OTP and socialite support
+     *
+     * @param  $controller
+     *
+     * @return void
+     */
     public function login($controller = LoginController::class)
     {
-        Route::middleware(['throttle:auth'])->group(function () use ($controller) {
+        $this->middleware(function () use ($controller) {
             Route::post('auth/login', [$controller, 'login']);
             Route::post('auth/login/otp-verify', [$controller, 'loginOTPVerify']);
             Route::post('auth/login/socialite/{driver}', [$controller, 'loginBySocialiteToken']);
         });
 
         // OTP throttle on sending code
-        Route::middleware(['throttle:otp'])->group(function () use ($controller) {
+        $this->otpMiddleware(function () use ($controller) {
             Route::post('auth/login/otp', [$controller, 'loginOTP']);
         });
     }
 
+    /**
+     * Logout route
+     *
+     * @param  $controller
+     *
+     * @return void
+     */
     public function logout($controller = LoginController::class)
     {
         Route::any('auth/logout', [$controller, 'logout']);
     }
 
+    /**
+     * OTP routes
+     *
+     * @param  $controller
+     *
+     * @return void
+     */
     public function otp($controller = OTPController::class)
     {
-        Route::middleware(['throttle:auth'])->group(function () use ($controller) {
+        $this->middleware(function () use ($controller) {
             Route::post('auth/otp/verify', [$controller, 'verify']);
         });
 
         // OTP throttle on sending code
-        Route::middleware(['throttle:otp'])->group(function () use ($controller) {
+        $this->otpMiddleware(function () use ($controller) {
             Route::post('auth/otp/resend', [$controller, 'resend']);
         });
     }
 
+    /**
+     * Register routes
+     *
+     * @param  $controller
+     *
+     * @return void
+     */
     public function register($controller = RegisterController::class)
     {
-        Route::middleware(['throttle:auth'])->group(function () use ($controller) {
+        $this->middleware(function () use ($controller) {
             Route::post('auth/register/otp-verify', [$controller, 'registerOTPVerify']);
         });
 
         // OTP throttle on sending code
-        Route::middleware(['throttle:otp'])->group(function () use ($controller) {
+        $this->otpMiddleware(function () use ($controller) {
             Route::post('auth/register/otp', [$controller, 'registerOTP']);
         });
     }
-}
+
+    /**
+     * Middleware for throttling
+     *
+     * @param  $callback
+     *
+     * @return void
+     */
+    public function middleware($callback)
+    {
+        Route::middleware(['throttle:auth'])->group(function () use ($callback) {
+            $callback();
+        });
+    }
+
+    /**
+     * Middleware for throttling OTP
+     *
+     * @param  $callback
+     *
+     * @return void
+     */
+    public function otpMiddleware($callback)
+    {
+        Route::middleware(['throttle:otp'])->group(function () use ($callback) {
+            $callback();
+        });
+    }
